6e931406c7e968d3600baf8d4d20a915ff4d1f41
BKadioglu <72888451+BKadioglu@users.noreply.github.com>
2022-01-21 11:56:57 +0300

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -1,9 +1,8 @@
 import streamlit as st
-
+from streamlit_player import st_player
 
 nav = st.sidebar.radio("Navigation",["Home","Linear Regression Model"])
 if nav == "Home":
-    video_file = open('first_vid', 'rb')
+    video_file = open(r'C:\Users\berke\OneDrive\Masaüstü\first_vid.mp4', 'rb')
     video_bytes = video_file.read()
-
     st.video(video_bytes)
