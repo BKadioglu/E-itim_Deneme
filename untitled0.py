3ef076c25026b9a40552c8a1873d41447b17f10e
BKadioglu <72888451+BKadioglu@users.noreply.github.com>
2022-01-21 12:31:17 +0300

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -3,10 +3,11 @@ import pandas as pd
 from pathlib import Path
 
 den = Path(__file__).parents[1] 
-print(den)
+st.write(den)
 nav = st.sidebar.radio("Navigation",["Home","1st Assignment"])
 if nav == "Home":
     st.title("Data Analyst Assignments")
+    st.write(den)
 elif nav == "1st Assignment":
    uploaded_files = st.file_uploader("Choose a CSV file", type="xlsx")
    if uploaded_files:
