7f94e94e7885843b9cceca2570ffb09893e8e58b
BKadioglu <72888451+BKadioglu@users.noreply.github.com>
2022-01-21 12:11:50 +0300

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -5,5 +5,5 @@ nav = st.sidebar.radio("Navigation",["Home","1st Assignment"])
 if nav == "Home":
     st.title("Data Analyst Assignments")
 elif nav == "1st Assignment":
-    df = pd.read_excel(r"C:\Users\berke\OneDrive\Masaüstü\den.xlsx")
+    df = pd.read_excel("den.xlsx")
     st.table(df)
